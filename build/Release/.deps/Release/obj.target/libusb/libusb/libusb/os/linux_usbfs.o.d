f777ac7fd8fc55fe80aa7a965081d583dd38d4ec
ratatatatat <rajiv.puri1192@gmail.com>
2016-07-21 18:13:41 -0400

    Testing blynk-library

---
+++ build/Release/.deps/Release/obj.target/libusb/libusb/libusb/os/linux_usbfs.o.d
@@ -0,0 +1,17 @@
+cmd_Release/obj.target/libusb/libusb/libusb/os/linux_usbfs.o := /home/rajiv/OpenWRT-Buildroot/openwrt/staging_dir/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-0.9.33.2/bin/mips-openwrt-linux-gcc '-DNODE_GYP_MODULE_NAME=libusb' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DENABLE_LOGGING=1' '-DDEFAULT_VISIBILITY=' '-DHAVE_GETTIMEOFDAY=1' '-DHAVE_POLL_H=1' '-DHAVE_SYS_TIME_H=1' '-DLIBUSB_DESCRIBE="1.0.17"' '-DPOLL_NFDS_TYPE=nfds_t' '-DTHREADS_POSIX=1' '-DOS_LINUX=1' '-D_GNU_SOURCE=1' '-DUSBI_TIMERFD_AVAILABLE=1' '-DHAVE_LIBUDEV=1' '-DUSE_UDEV=1' -I/home/rajiv/.node-gyp/4.3.1/include/node -I/home/rajiv/.node-gyp/4.3.1/src -I/home/rajiv/.node-gyp/4.3.1/deps/uv/include -I/home/rajiv/.node-gyp/4.3.1/deps/v8/include -I../libusb_config -I../libusb/libusb -I../libusb/libusb/os  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -w -O3 -ffunction-sections -fdata-sections -fno-omit-frame-pointer  -MMD -MF ./Release/.deps/Release/obj.target/libusb/libusb/libusb/os/linux_usbfs.o.d.raw -Os -pipe -mno-branch-likely -mips32r2 -mtune=34kc -fno-caller-saves -fhonour-copts -Wno-error=unused-but-set-variable -Wno-error=unused-result -msoft-float -mips16 -minterlink-mips16 -fpic -I /home/rajiv/OpenWRT-Buildroot/openwrt/staging_dir/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-0.9.33.2/usr/include -I /home/rajiv/OpenWRT-Buildroot/openwrt/staging_dir/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-0.9.33.2/include -I /home/rajiv/OpenWRT-Buildroot/openwrt/staging_dir/target-mips_34kc_uClibc-0.9.33.2/usr/include -I /home/rajiv/OpenWRT-Buildroot/openwrt/staging_dir/target-mips_34kc_uClibc-0.9.33.2/include -c -o Release/obj.target/libusb/libusb/libusb/os/linux_usbfs.o ../libusb/libusb/os/linux_usbfs.c
+Release/obj.target/libusb/libusb/libusb/os/linux_usbfs.o: \
+ ../libusb/libusb/os/linux_usbfs.c ../libusb_config/config.h \
+ ../libusb/libusb/libusb.h ../libusb/libusb/libusbi.h \
+ ../libusb/libusb/libusb.h ../libusb/libusb/version.h \
+ ../libusb/libusb/version_nano.h ../libusb/libusb/os/threads_posix.h \
+ ../libusb/libusb/os/poll_posix.h ../libusb/libusb/os/linux_usbfs.h
+../libusb/libusb/os/linux_usbfs.c:
+../libusb_config/config.h:
+../libusb/libusb/libusb.h:
+../libusb/libusb/libusbi.h:
+../libusb/libusb/libusb.h:
+../libusb/libusb/version.h:
+../libusb/libusb/version_nano.h:
+../libusb/libusb/os/threads_posix.h:
+../libusb/libusb/os/poll_posix.h:
+../libusb/libusb/os/linux_usbfs.h:
